91261cb3467d092c3e13b2c2cacd03f5211da936
Ritesh Bafna <Riteshbafna025@gmail.com>
2025-05-18 18:44:17 +0530

    Final commit

--- components/Helper/SectionHeading.tsx
+++ components/Helper/SectionHeading.tsx
@@ -9,7 +9,7 @@ const SectionHeading = ({heading}: Props) => {
     <div className='w-[80%] mx-auto'>
       <h1 className='text-xl sm:text-3xl text-emerald-900 font-bold'> {heading}</h1>
       <p className='mt-2 text-gray-800 sm:text-base text-sm font-medium'>
-        The best choice for your next trip, Traip helps you find the perfect trip, whether you're planning a weekend getaway, a family vacation, or a business trip. 
+        The best choice for your next trip, Traip helps you find the perfect trip, whether you are planning a weekend getaway, a family vacation, or a business trip. 
       </p>
     </div>
   )
